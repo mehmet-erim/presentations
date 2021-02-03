ff90311dfd99b3b89344e7e884f434e2c45dd7f8
mehmet-erim <bilgi@mehmeterim.com>
2021-02-04 00:51:37 +0300

    create counter component

---
+++ Angular Syntax/src/app/counter/counter.component.ts
@@ -0,0 +1,33 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  template: `
+    <button class="btn" (click)="decrement()">-</button>
+    <span style="font-size: 30px; margin-left: 10px; margin-right: 10px">{{
+      count
+    }}</span>
+    <button class="btn" (click)="increment()">+</button>
+  `,
+  styleUrls: ['./counter.component.scss'],
+})
+export class CounterComponent {
+  @Output() countChange = new EventEmitter<number>();
+  private _count = 0;
+  @Input()
+  set count(value: number) {
+    this._count = value;
+    this.countChange.emit(value);
+  }
+  get count() {
+    return this._count;
+  }
+
+  increment() {
+    this.count = this.count + 1;
+  }
+
+  decrement() {
+    this.count = this.count - 1;
+  }
+}
